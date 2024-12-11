21477039543cef0f6a512ff7555040444c74c4b0
Bassist_Zero <bassist.zero@icloud.com>
2024-12-11 13:47:39 +0300

    Update `update.sh`

--- update.sh
+++ update.sh
@@ -1,3 +1,19 @@
+tag=6.1.0
+
+# Download
 ./download-specific-version.sh \
 	-l https://github.com/hyperoslo/Whisper.git \
-	-t 6.1.0
+	-t $tag
+
+# Configure `Sources` folder
+cd Sources
+mv SupportFiles/Info.plist Source
+mv .gitignore Source
+mv Source ..
+cd ..
+rm -rf Sources
+mv Source Sources
+
+# Git stuff
+git commit -m "Update to $tag version"
+git tag $tag HEAD
